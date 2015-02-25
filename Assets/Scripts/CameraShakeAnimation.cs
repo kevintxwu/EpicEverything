2c62fd6b3cfe268a29908324186744f23652c041
John Du <johnldu@gmail.com>
2015-02-24 23:47:17 -0800

    commit part 2

---
+++ Assets/Scripts/CameraShakeAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeAnimation : MonoBehaviour {
+
+    private float shakeIntensity;
+    private float shakeDecay = 0.1f;
+
+    public void Animate(int damage) {
+        shakeIntensity = damage * 1f / 5f;
+    }
+
+    void Update() {
+        if (shakeIntensity >= 0) {
+            camera.transform.position = new Vector3(
+                Random.Range(-shakeIntensity, shakeIntensity),
+                camera.transform.position.y,
+                Random.Range(-shakeIntensity, shakeIntensity));
+            shakeIntensity -= shakeDecay;
+        }
+    }
+}
